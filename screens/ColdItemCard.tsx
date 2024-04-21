b928f8fbde37c6120e3ab0b7dbe2b2e34d564de2
ziaxgit <112583896+ziaxgit@users.noreply.github.com>
2024-04-21 15:33:21 +0100

    add increment button with haptic feedback

--- screens/ColdItemCard.tsx
+++ screens/ColdItemCard.tsx
@@ -1,38 +1,61 @@
 // ColdItemCard.tsx
-import React from "react";
-import { View, Text, TextInput } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import { useItemsToOrder } from "./ItemsToOrderContext";
 import { AntDesign } from "@expo/vector-icons";
+import * as Haptic from "expo-haptics";
 
 interface Props {
   itemName: string;
   itemQuantity: string;
 }
 
-const ColdItemCard: React.FC<Props> = ({ itemName, itemQuantity }) => {
+const ColdItemCard: React.FC<Props> = ({ itemName }) => {
   const { addColdItemToOrder } = useItemsToOrder();
+  const [quantity, setQuantity] = useState("0");
 
   const handleQuantityChange = (value: string) => {
-    const item = { name: itemName, quantity: value };
+    const item = { name: itemName, quantity: quantity };
     addColdItemToOrder(item);
   };
 
+  const incrementQuantity = () => {
+    Haptic.selectionAsync(); // Add haptic feedback
+
+    const newQuantity = parseInt(quantity) + 1; // Increment quantity
+    setQuantity(newQuantity.toString()); // Update state
+    const item = { name: itemName, quantity: newQuantity.toString() }; // Use the updated quantity
+    addColdItemToOrder(item);
+  };
+
+  const decrementQuantity = () => {
+    setQuantity((prevQuantity) => {
+      const newQuantity = parseInt(prevQuantity) - 1;
+      return newQuantity < 0 ? "0" : newQuantity.toString();
+    });
+  };
+
   return (
-    <View className="flex-row justify-between shadow-2xl bg-slate-50 my-1">
+    <View className="flex-row justify-between shadow-2xl bg-slate-50 p-2 bg-green-100">
       <View className="justify-center">
         <Text>{itemName}</Text>
       </View>
-      <View className="flex-row gap-1">
-        <AntDesign name="pluscircleo" size={24} color="green" />
+      <View className="flex-row gap-1 mr-2">
+        <TouchableOpacity onPress={decrementQuantity}>
+          <AntDesign name="minuscircleo" size={20} color="red" />
+        </TouchableOpacity>
+
         <TextInput
           className="bg-white w-7 text-center"
-          value={itemQuantity}
-          onChangeText={handleQuantityChange}
+          // onChangeText={handleQuantityChange}
           keyboardType="numeric"
           placeholder="0"
           returnKeyType="done"
+          value={quantity}
         />
-        <AntDesign name="minuscircleo" size={24} color="red" />
+        <TouchableOpacity onPress={incrementQuantity}>
+          <AntDesign name="pluscircleo" size={20} color="green" />
+        </TouchableOpacity>
       </View>
     </View>
   );
