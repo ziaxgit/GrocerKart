7381126a6dcf8204b02430ab4f1f72c0adb8874d
ziaxgit <112583896+ziaxgit@users.noreply.github.com>
2024-04-24 19:58:36 +0100

    add Modal to view order

--- screens/OrderDetails.tsx
+++ screens/OrderDetails.tsx
@@ -1,39 +1,41 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Modal, TouchableOpacity } from "react-native";
 import { useItemsToOrder } from "./ItemsToOrderContext";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 const OrderDetails: React.FC = () => {
   const { coldItems, dryItems } = useItemsToOrder();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  // Function to generate a string of items separated by a line break
+  const generateItemsString = (items) => {
+    return items.map((item) => `${item.quantity} x ${item.name}`).join("\n");
+  };
+
+  // Generate strings for cold and dry items
+  const coldItemsString = generateItemsString(coldItems);
+  const dryItemsString = generateItemsString(dryItems);
+
+  // Add an extra line break between cold and dry items
+  const combinedItemsString = coldItemsString + "\n\n" + dryItemsString;
 
   return (
-    <View>
-      <Text
-        style={{
-          fontSize: 18,
-          fontWeight: "bold",
-          marginBottom: 10,
-          marginHorizontal: 2,
-        }}
-      >
-        Current Order:
-      </Text>
-      <View>
-        <Text style={{ fontWeight: "bold" }}>Cold Items:</Text>
-        {coldItems.map((item, index) => (
-          <Text key={index} className="mx-2">
-            {item.quantity} x {item.name}
-          </Text>
-        ))}
-      </View>
-      <View>
-        <Text style={{ fontWeight: "bold" }}>Dry Items:</Text>
-        {dryItems.map((item, index) => (
-          <Text key={index} className="mx-2">
-            {item.quantity} x {item.name}
-          </Text>
-        ))}
-      </View>
-    </View>
+    <Modal>
+      <SafeAreaProvider>
+        <SafeAreaView>
+          <View>
+            <Text>ORDER DETAILS</Text>
+            <Text>{combinedItemsString}</Text>
+          </View>
+          <TouchableOpacity>
+            <Text>COPY ORDER</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => setIsModalVisible(false)}>
+            <Text>CLOSE</Text>
+          </TouchableOpacity>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </Modal>
   );
 };
 
